89bb0e8e54d06b783cba9a5b8f67e509e8c34955
chichoon <chichoon.choi@gmail.com>
2022-11-02 23:11:28 +0900

    fix: alter user 순서 변경

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -18,9 +18,9 @@ cat /var/lib/mysql/.setup 2> /dev/null;
 # 그와 함께 $? errno에 값이 설정됨
 
 if [ $? -ne 0 ]; then
-    mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW'; FLUSH PRIVILEGES" # 루트 계정 비밀번호 초기화
     mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB"; # 새로운 DB 생성 (기존에 존재하지 않을 경우)
     mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW'"; # 유저 생성 (기존에 존재하지 않을 경우)
+    mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW'; FLUSH PRIVILEGES" # 루트 계정 비밀번호 초기화
     mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW'"; # 생성된 유저에 DB의 모든 권한 부여
     mysql $MYSQL_DB -u$MYSQL_ROOT -p$MYSQL_ROOT_PW; #루트 계정으로 로그인
     touch /var/lib/mysql/.setup
