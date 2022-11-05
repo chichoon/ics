dcd9d8422497890c564090e4c97bf0c535d9df4c
chichoon <chichoon.choi@gmail.com>
2022-11-05 22:52:39 +0900

    feat: wordpress while문 until로 수정해봄

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -8,7 +8,10 @@ echo " __        __            _ ____
 
 "
 
-while ! mariadb -u$MYSQL_USER -p$MYSQL_PW &> /dev/null; do
+until mariadb -u$MYSQL_USER -p$MYSQL_PW &> /dev/null
+do
 	echo "waiting for MariaDB to be on..."
 	sleep 1
 done
+# mariadb 로그인에 실패할 경우 sleep 1
+# &> 은 오류 (stderr) 를 리디렉션한다
