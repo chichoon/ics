e519de8ced67865261e7d24f605774c6b37cd831
chichoon <chichoon.choi@gmail.com>
2022-11-05 22:49:08 +0900

    feat: setup.sh 내용 추가 (임시)

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,1 +1,14 @@
-# setup
+#!/bin/sh
+
+echo " __        __            _ ____
+ \\ \\      / /__  _ __ __| |  _ \\ _ __ ___  ___ ___
+  \ \ /\ / / _ \| '__/ _' | |_) | '__/ _ \/ __/ __|
+   \ V  V / (_) | | | (_| |  __/| | |  __/\__ \__ \\
+    \_/\_/ \___/|_|  \__,_|_|   |_|  \___||___/___/\
+
+"
+
+while ! mariadb -u$MYSQL_USER -p$MYSQL_PW &> /dev/null; do
+	echo "waiting for MariaDB to be on..."
+	sleep 1
+done
