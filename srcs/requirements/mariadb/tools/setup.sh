b24bb40dfca9ac06e692beabd5712388c8d1d285
chichoon <chichoon.choi@gmail.com>
2022-11-10 22:56:15 +0900

    fix: 루트권한 바꾸는 부분 위로 끌올

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -29,11 +29,12 @@ cat /var/lib/mysql/.setup &> /dev/null;
 # 그와 함께 $? errno에 값이 설정됨
 
 if [ $? -ne 0 ]; then # $?이 0이 아닐 경우 (not equal) => 에러가 났다는 뜻 => .setup이 존재하지 않으므로 새로 만들어야 함
+    mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW'"; # 루트 계정 비밀번호 초기화
+    mysql -e "FLUSH PRIVILEGES"; # 그리고 권한 변경사항을 전부 즉시 적용
     mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB"; # 새로운 DB 생성 (기존에 존재하지 않을 경우)
     mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW'"; # 유저 생성 (기존에 존재하지 않을 경우)
     mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW'"; # 생성된 유저에 DB (내의 모든 테이블) 의 모든 권한 부여 (원격 접속 권한 포함)
     mysql -e "FLUSH PRIVILEGES"; # 그리고 권한 변경사항을 전부 즉시 적용
-    mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW'"; # 루트 계정 비밀번호 초기화
     mysql $MYSQL_DB -u$MYSQL_ROOT -p$MYSQL_ROOT_PW; #루트 계정으로 로그인
     touch /var/lib/mysql/.setup;
 fi
