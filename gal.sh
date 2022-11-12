8b92cfde6867a66ad1d2ab7eeeb808dbd786c942
chichoon <chichoon.choi@gmail.com>
2022-11-12 10:45:56 +0900

    fix: gal.sh 볼륨 경로 변경

--- gal.sh
+++ gal.sh
@@ -4,4 +4,4 @@ docker stop $(docker ps -qa); docker rm $(docker ps -qa);
 docker rmi -f $(docker images -qa); docker volume rm $(docker volume ls -q);
 docker network rm $(docker network ls -q) 2>/dev/null
 rm -rf /home/jiychoi/data/wordpress/*
-rm -rf /home/jiychoi/data/mysql/*
+rm -rf /home/jiychoi/data/mariadb/*
