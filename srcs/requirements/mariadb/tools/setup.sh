eb68cc2ca7b411301d95fbb5729d2888a34f94fa
chichoon <chichoon.choi@gmail.com>
2022-11-14 23:00:04 +0900

    fix: test 완료

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -6,7 +6,7 @@
 #    By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/18 10:59:37 by jiychoi           #+#    #+#              #
-#    Updated: 2022/11/21 16:32:23 by jiychoi          ###   ########.fr        #
+#    Updated: 2022/11/22 03:27:00 by jiychoi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,15 +22,13 @@ echo "                       _       _____  ____
 "
 chown -R mysql:mysql /var/lib/mysql; # /var/lib/mysql (mysql 설치 경로) 에 mysql 유저 (mysql 그룹) 에 대한 권한 부여
 
-service mysql start;
-# MySQL 서비스 시작 (mariaDB는 MySQL의 포크판)
-
 cat /var/lib/mysql/.setup 2> /dev/null;
 # /var/lib/mysql 폴더 (볼륨) 에 .setup 파일이 있을 경우 에러 X
 # .setup 파일이 존재하지 않을 경우 에러 발생 => 내용물은 쓰레기통으로
 # 그와 함께 $? errno에 값이 설정됨
 
 if [ $? -ne 0 ]; then # $?이 0이 아닐 경우 (not equal) => 에러가 났다는 뜻 => .setup이 존재하지 않으므로 새로 만들어야 함
+    service mysql start; # MySQL 서비스 시작 (mariaDB는 MySQL의 포크판)
     mysql -hlocalhost -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB"; # 새로운 DB 생성 (기존에 존재하지 않을 경우)
     mysql -hlocalhost -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW'"; # 유저 생성 (기존에 존재하지 않을 경우)
     mysql -hlocalhost -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%'"; # 생성된 유저에 DB (내의 모든 테이블) 의 모든 권한 부여 (원격 접속 권한 포함)
@@ -43,4 +41,4 @@ if [ $? -ne 0 ]; then # $?이 0이 아닐 경우 (not equal) => 에러가 났다
     touch /var/lib/mysql/.setup;
 fi
 
-exec mysqld --console;
+exec service mysql start | mysqld --console;
